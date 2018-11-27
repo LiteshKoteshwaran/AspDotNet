91de484b662b758b2e59f0d693ee1cc729c2fbd9
Litesh Koteshawaran <44187300+LiteshKoteshwaran@users.noreply.github.com>
2018-11-27 09:53:59 +0530

    for review

--- Practice/26Nov/26Nov/Site.Master.designer.cs
+++ Practice/26Nov/26Nov/Site.Master.designer.cs
@@ -13,15 +13,6 @@ namespace _26Nov {
     public partial class SiteMaster {
         
         /// <summary>
-        /// ProIcon control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlAnchor ProIcon;
-        
-        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
